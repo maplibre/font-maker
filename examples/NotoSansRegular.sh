2c2085a027b0062c4e273d4d9f22fd14c326f6ee
Brandon Liu <bdon@bdon.org>
2020-08-09 17:19:49 -0400

    add examples

---
+++ examples/NotoSansRegular.sh
@@ -0,0 +1,14 @@
+../sdf-glyph pbfs/NotoSansRegular \
+fonts/Noto-unhinted/NotoSans-Regular.ttf \
+fonts/Noto-unhinted/NotoSansCJKtc-Regular.otf \
+fonts/Noto-unhinted/NotoSansArabic-Regular.ttf \
+fonts/Noto-unhinted/NotoSansEthiopic-Regular.ttf \
+fonts/Noto-unhinted/NotoSansHebrew-Regular.ttf \
+fonts/Noto-unhinted/NotoSansThai-Regular.ttf \
+fonts/Noto-unhinted/NotoSansThaana-Regular.ttf \
+fonts/Noto-unhinted/NotoSansMyanmar-Regular.ttf \
+fonts/Noto-unhinted/NotoSansBengali-Regular.ttf \
+fonts/Noto-unhinted/NotoSansTibetan-Regular.ttf \
+fonts/Noto-unhinted/NotoSansLao-Regular.ttf \
+fonts/Noto-unhinted/NotoSansKhmer-Regular.ttf \
+fonts/Noto-unhinted/NotoSansCanadianAboriginal-Regular.ttf

710c5e3f5b18d564ae90a21c362b977199de9ddc
Brandon Liu <bdon@bdon.org>
2020-08-24 23:05:00 -0400

    add Noto Sans Bold example

---
+++ examples/NotoSansBold.sh
@@ -0,0 +1,27 @@
+../sdf-glyph pbfs/NotoSansBold \
+fonts/Noto-unhinted/NotoSans-Bold.ttf \
+fonts/Noto-unhinted/NotoSansArmenian-Bold.ttf \
+fonts/Noto-unhinted/NotoSansHebrew-Bold.ttf \
+fonts/Noto-unhinted/NotoSansArabic-Bold.ttf \
+fonts/Noto-unhinted/NotoSansThaana-Bold.ttf \
+fonts/Noto-unhinted/NotoSansDevanagari-Bold.ttf \
+fonts/Noto-unhinted/NotoSansBengali-Bold.ttf \
+fonts/Noto-unhinted/NotoSansGurmukhi-Bold.ttf \
+fonts/Noto-unhinted/NotoSansGujarati-Bold.ttf \
+fonts/Noto-unhinted/NotoSansOriya-Bold.ttf \
+fonts/Noto-unhinted/NotoSansTamil-Bold.ttf \
+fonts/Noto-unhinted/NotoSansTelugu-Bold.ttf \
+fonts/Noto-unhinted/NotoSansKannada-Bold.ttf \
+fonts/Noto-unhinted/NotoSansMalayalam-Bold.ttf \
+fonts/Noto-unhinted/NotoSansSinhala-Bold.ttf \
+fonts/Noto-unhinted/NotoSansThai-Bold.ttf \
+fonts/Noto-unhinted/NotoSansLao-Bold.ttf \
+fonts/Noto-unhinted/NotoSansTibetan-Bold.ttf \
+fonts/Noto-unhinted/NotoSansMyanmar-Bold.ttf \
+fonts/Noto-unhinted/NotoSansGeorgian-Bold.ttf \
+fonts/Noto-unhinted/NotoSansEthiopic-Bold.ttf \
+fonts/Noto-unhinted/NotoSansCherokee-Bold.ttf \
+fonts/Noto-unhinted/NotoSansKhmer-Bold.ttf \
+fonts/Noto-unhinted/NotoSansSymbols-Bold.ttf \
+fonts/Noto-unhinted/NotoSansCJKtc-Bold.otf \
+fonts/Noto-unhinted/NotoSansCham-Bold.ttf

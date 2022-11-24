5b5168c4a434b7d3cee23878b36364788c9a61ed
Brandon Liu <bdon@bdon.org>
2022-11-24 13:16:34 +0800

    ci

--- build.sh
+++ build.sh
@@ -3,7 +3,7 @@ emcc -I vendor/sdf-glyph-foundry/include/ \
 		 -I vendor/protozero/include \
 		 -s USE_FREETYPE=1 \
 		 -s EXPORTED_RUNTIME_METHODS=[ccall,cwrap] \
-		 -s EXPORTED_FUNCTIONS=_generate_range \
+		 -s EXPORTED_FUNCTIONS=[_generate_range] \
 		 -o js/sdfglyph.js \
 		 -Wno-enum-constexpr-conversion \
 		 main.cpp

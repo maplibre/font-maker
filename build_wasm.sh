bc00ef4f9465edbd964e9bf57e6b132f95fcc5ab
Brandon Liu <bdon@users.noreply.github.com>
2022-12-16 10:28:47 +0800

    wasm build set ALLOW_MEMORY_GROWTH [#1] (#15)

--- build_wasm.sh
+++ build_wasm.sh
@@ -10,6 +10,7 @@ emcc -I vendor/sdf-glyph-foundry/include/ \
 		 -s USE_FREETYPE=1 \
 		 -s EXPORTED_RUNTIME_METHODS=[ccall] \
 		 -s EXPORTED_FUNCTIONS=[_generate_glyph_buffer,_free_glyph_buffer,_create_fontstack,_free_fontstack,_fontstack_name,_glyph_buffer_data,_glyph_buffer_size,_fontstack_add_face] \
+		 -s ALLOW_MEMORY_GROWTH=1 \
 		 -o app/public/sdfglyph.js \
 		 -Wno-enum-constexpr-conversion \
 		 main.cpp

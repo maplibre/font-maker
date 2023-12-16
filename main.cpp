ece2f4a1d9781d6793efe5b03fa79b5c7a165a42
Chris Boone <cboone@users.noreply.github.com>
2023-12-16 10:13:30 -0500

    Improve CLI help

--- main.cpp
+++ main.cpp
@@ -203,16 +203,23 @@ extern "C" {
 #ifndef EMSCRIPTEN
 int main(int argc, char *argv[])
 {
-    cxxopts::Options cmd_options("font-maker", "Create font PBFs.");
+    cxxopts::Options cmd_options("font-maker", "Create font PBFs from TTFs or OTFs.");
     cmd_options.add_options()
-        ("output", "Output directory", cxxopts::value<string>())
-        ("fonts", "Input fonts TTF or OTF", cxxopts::value<vector<string>>())
+        ("output", "Output directory (to be created, must not already exist)", cxxopts::value<string>())
+        ("fonts", "Input font(s) (as TTF or OTF)", cxxopts::value<vector<string>>())
         ("name", "Override output fontstack name", cxxopts::value<string>())
+        ("help", "Print usage")
     ;
+    cmd_options.positional_help("<OUTPUT_DIR> <INPUT_FONT> [INPUT_FONT2 ...]");
     cmd_options.parse_positional({"output","fonts"});
     auto result = cmd_options.parse(argc, argv);
+    if (result.count("help"))
+    {
+      cout << cmd_options.help() << endl;
+      exit(0);
+    }
     if (result.count("output") == 0 || result.count("fonts") == 0) {
-        cout << "usage: font-maker OUTPUT_DIR INPUT_FONT [INPUT_FONT2 ...]" << endl;
+        cout << cmd_options.help() << endl;
         exit(1);
     }
     auto output_dir = result["output"].as<string>();
